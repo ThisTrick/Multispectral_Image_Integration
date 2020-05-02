e34f2a0f1f735ac7aa8579efa6e62315d6e589be
ThisTrick <way.trickster@gmail.com>
2020-05-02 16:22:13 +0300

    Изменен класс InterlacingMaximumMethodFusion

--- Multispectral_Image_Integration_Library/InterlacingMaximumMethodFusion.cs
+++ Multispectral_Image_Integration_Library/InterlacingMaximumMethodFusion.cs
@@ -6,26 +6,12 @@ namespace Multispectral_Image_Integration_Library
     /// Метод слияния двух изображений основанный на том что в каждую n+1 строку добавляются пиксели максимальной интенсивности.
     /// Где строка n - строка изображения img1, строка n+1 - строка изображения img2.
     /// </summary>
-    public class InterlacingMaximumMethodFusion : IImageFusion
+    public class InterlacingMaximumMethodFusion : PixelByPixelFusion, IImageFusion
     {
         public FastBitmap Fusion(FastBitmap img1, FastBitmap img2)
         {
-            if (img1.Width != img2.Width || img2.Height != img2.Height)
-            {
-                throw new ArgumentException("Изображения должны быть одного размера");
-            }
-            var imgResult = img1.Clone();
-            for (int x = 0; x < imgResult.Width; x++)
-            {
-                for (int y = 0; y < imgResult.Height; y += 2)
-                {
-                    for (int color = 0; color < imgResult.DimensionsColor; color++)
-                    {
-                        imgResult[x, y, color] = img1[x, y, color] > img2[x, y, color] ? img1[x, y, color] : img2[x, y, color];
-                    }
-                }
-            }
-            return imgResult;
+            Func<byte, byte, byte> method = (pixel1, pixel2) => pixel1 > pixel2 ? pixel1 : pixel2;
+            return PixelFusion(img1, img2, method, 2);
         }
     }
 }
